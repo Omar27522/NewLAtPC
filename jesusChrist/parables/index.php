57c9876b725295aff857f01d987fd1d278fc585c
MiguelOmar GD <m1i6g5u9e8l2@gmail.com>
2025-04-25 22:24:14 -0700

    The parables are done, I just need to integrate them completely, clean up, and organize a bit.

---
+++ jesusChrist/parables/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'The Parables';
+$dir = '../../';
+include_once('../../code/functions.php');
+include './content.php';
+//include '../content/heavenlyFatherTellmeAboutTheWeb.php';
+include ('../../code/html_structure/page.php');
+?>
